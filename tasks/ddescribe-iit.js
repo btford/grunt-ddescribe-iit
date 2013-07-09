ee4811c6aa0d19de714f23555780fbd5a007d3f9
Brian Ford <btford@umich.edu>
2013-07-08 17:23:02 -0700

    refactor to use async APIs

--- tasks/ddescribe-iit.js
+++ tasks/ddescribe-iit.js
@@ -6,26 +6,25 @@ module.exports = function (grunt) {
 
   grunt.registerMultiTask('ddescribe-iit', 'Check for instances of ddescribe and iit', function () {
     var failed = false;
-    this.files.forEach(function (glob) {
-      glob.src
-        .map(function (file) {
-          return {
-            name: file,
-            contents: grunt.file.read(file)
-          };
-        })
-        .forEach(function (file) {
-          var errs;
-          if (errs = checkFile(file.contents)) {
-            errs.forEach(function (err) {
-              grunt.log.errorlns(file.name + ' has `' + err.str + '` at line ' + err.line);
-            });
-            failed = true;
-          }
-        });
+
+    var done = this.async();
+    grunt.util.async.forEach(this.filesSrc, function (file, next) {
+      if (grunt.file.isFile(file)) {
+        var fileContents = grunt.file.read(file);
+        var errs;
+        if (errs = checkFile(fileContents)) {
+          errs.forEach(function (err) {
+            grunt.log.errorlns(file + ' has `' + err.str + '` at line ' + err.line);
+          });
+          next(true);
+        }
+      }
+      next();
+    }, function (failed) {
+      if (failed) {
+        grunt.warn('ddescribe-iit check failed.');
+      }
+      done();
     });
-    if (failed) {
-      grunt.fail.fatal('ddescribe-iit check failed.');
-    }
   });
 };
