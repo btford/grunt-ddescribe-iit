d43a3f6a357a4a72dc73723c07daa07e21c68dd1
Eitan Peer <eitan.peer@hp.com>
2014-03-24 16:42:03 +0200

    Add support for custom disallowed keywords

--- tests/simple.js
+++ tests/simple.js
@@ -6,47 +6,58 @@ var checkFile = require('../tasks/lib/check-file');
 
 describe('check-file', function () {
 
+  var defaultDisallowed = ["iit", "xit", "ddescribe", "xdescribe"];
   it('should return undefined if there are no problems', function () {
-    should.not.exist(checkFile("it('is a-ok')"));
+    should.not.exist(checkFile("it('is a-ok')", defaultDisallowed));
   });
 
   it('should return an array if there is `iit`', function () {
-    should.exist(checkFile("iit('is not a-ok')"));
+    should.exist(checkFile("iit('is not a-ok')", defaultDisallowed));
   });
 
   it('should return an array if there is `ddescribe`', function () {
-    should.exist(checkFile("ddescribe('is not a-ok')"));
+    should.exist(checkFile("ddescribe('is not a-ok')", defaultDisallowed));
   });
 
   it('should return an array if there is `xit`', function () {
-    should.exist(checkFile("xit('is not a-ok')"));
+    should.exist(checkFile("xit('is not a-ok')", defaultDisallowed));
   });
 
   it('should return an array if there is `xit` after `exit`', function () {
-    should.exist(checkFile("exit(0);xit('is not a-ok')"));
-    should.exist(checkFile("it('is a-ok');exit(0);xit('is not a-ok')"));
+    should.exist(checkFile("exit(0);xit('is not a-ok')", defaultDisallowed));
+    should.exist(checkFile("it('is a-ok');exit(0);xit('is not a-ok')", defaultDisallowed));
   });
 
   it('should return an array if there is `xdescribe`', function () {
-    should.exist(checkFile("xdescribe('is not a-ok')"));
+    should.exist(checkFile("xdescribe('is not a-ok')", defaultDisallowed));
   });
 
   it('should give the line number of the problem', function () {
-    var problems = checkFile("ddescribe('is not a-ok')");
+    var problems = checkFile("ddescribe('is not a-ok')", defaultDisallowed);
     problems.length.should.equal(1);
     should.equal(problems[0].line, 1);
 
-    problems = checkFile("\n\niit('is not a-ok')");
+    problems = checkFile("\n\niit('is not a-ok')", defaultDisallowed);
     problems.length.should.equal(1);
     should.equal(problems[0].line, 3);
 
-    problems = checkFile("\n\n\n\n\nxit('is not a-ok')");
+    problems = checkFile("\n\n\n\n\nxit('is not a-ok')", defaultDisallowed);
     problems.length.should.equal(1);
     should.equal(problems[0].line, 6);
   });
 
   it('should not treat `exit` as `xit`', function () {
-    should.not.exist(checkFile("exit()"));
+    should.not.exist(checkFile("exit()", defaultDisallowed));
+  });
+
+  it('can use cutom disallowed keywords', function () {
+	  should.not.exist(checkFile("xit('is a-ok to use custom disallowed keywords')", ["iit", "ddescribe"]));
+  });
+
+  it('passes if there are no disallowed keywords', function () {
+	  should.not.exist(checkFile("xit('is a-ok to use custom disallowed keywords')"));
+	  should.not.exist(checkFile("xit('is a-ok to use custom disallowed keywords')", []));
+	  should.not.exist(checkFile("xit('is a-ok to use custom disallowed keywords')", {}));
   });
 
 });
